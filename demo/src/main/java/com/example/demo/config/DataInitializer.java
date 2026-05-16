8ebd81030302ad3c74355c857f25dc6e3601747b
Ragil Pasaribu <pasariburagil2@gmail.com>
2026-05-16 14:39:48 +0900

    move pass admin to env

--- demo/src/main/java/com/example/demo/config/DataInitializer.java
+++ demo/src/main/java/com/example/demo/config/DataInitializer.java
@@ -11,25 +11,31 @@ import com.example.demo.entity.Status;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class DataInitializer  {
+public class DataInitializer {
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    private final Dotenv dotenv = Dotenv.load();
 
     @Bean
     CommandLineRunner initAdmin() {
         return args -> {
 
-            
-            if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
+            String adminEmail = dotenv.get("ADMIN_EMAIL");
+            String adminPassword = dotenv.get("ADMIN_PASSWORD");
+
+            if (userRepository.findByEmail(adminEmail).isEmpty()) {
 
                 User admin = User.builder()
                         .name("Admin")
-                        .email("admin@gmail.com")
-                        .password(passwordEncoder.encode("Admin@123")) 
+                        .email(adminEmail)
+                        .password(passwordEncoder.encode(adminPassword))
                         .role(Role.ADMIN)
                         .status(Status.APPROVED)
                         .programStudi(ProgramStudi.TEKNIK_INFORMATIKA)
@@ -40,5 +46,5 @@ public class DataInitializer  {
                 System.out.println("Admin default berhasil dibuat");
             }
         };
-    } 
+    }
 }
